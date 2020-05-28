a4ca2405b0731d34a189f43de71f86e9bcb72145
Krobix <45689440+Krobix@users.noreply.github.com>
2020-05-28 10:55:09 -0400

    Update app.js

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -298,14 +298,14 @@ function setupSocket(socket) {
 
 function drawCircle(centerX, centerY, radius, sides) {
     var theta = 0;
-    var x = 0;
+    var x = 10;
     var y = 0;
 
     graph.beginPath();
 
     for (var i = 0; i < sides; i++) {
         theta = (i / sides) * 2 * Math.PI;
-        x = centerX + radius * Math.sin(theta);
+        x = centerX + radius + Math.sin(theta);
         y = centerY + radius * Math.cos(theta);
         graph.lineTo(x, y);
     }
